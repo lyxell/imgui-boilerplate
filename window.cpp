366e9a0daeef28c693c360f01c22d87c4f8f0bcf
lyxell <alyxell@kth.se>
2021-04-17 13:43:04 +0200

    Fix line width

--- window.cpp
+++ window.cpp
@@ -1,9 +1,13 @@
 #include "window.hpp"
 
 // Dear ImGui: standalone example application for SDL2 + OpenGL
-// (SDL is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
-// (GL3W is a helper library to access OpenGL functions since there is no standard header to access modern OpenGL functions easily. Alternatives are GLEW, Glad, etc.)
-// If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
+// (SDL is a cross-platform general purpose library for handling windows,
+// inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
+// (GL3W is a helper library to access OpenGL functions since there is no
+// standard header to access modern OpenGL functions easily. Alternatives are
+// GLEW, Glad, etc.)
+// If you are new to Dear ImGui, read documentation from the docs/ folder +
+// read the top of imgui.cpp.
 // Read online: https://github.com/ocornut/imgui/tree/master/docs
 
 #include "imgui.h"
@@ -15,25 +19,36 @@
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <GLES2/gl2.h>
 // About Desktop OpenGL function loaders:
-//  Modern desktop OpenGL doesn't have a standard portable header file to load OpenGL function pointers.
-//  Helper libraries are often used for this purpose! Here we are supporting a few common ones (gl3w, glew, glad).
-//  You may use another loader/header of your choice (glext, glLoadGen, etc.), or chose to manually implement your own.
+//  Modern desktop OpenGL doesn't have a standard portable header file to load
+//  OpenGL function pointers.
+//  Helper libraries are often used for this purpose! Here we are supporting a
+//  few common ones (gl3w, glew, glad).
+//  You may use another loader/header of your choice (glext, glLoadGen, etc.),
+//  or chose to manually implement your own.
 #elif defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
-#include <GL/gl3w.h>            // Initialize with gl3wInit()
+// Initialize with gl3wInit()
+#include <GL/gl3w.h>
 #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
-#include <GL/glew.h>            // Initialize with glewInit()
+// Initialize with glewInit()
+#include <GL/glew.h>
 #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
-#include <glad/glad.h>          // Initialize with gladLoadGL()
+// Initialize with gladLoadGL()
+#include <glad/glad.h>
 #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
-#include <glad/gl.h>            // Initialize with gladLoadGL(...) or gladLoaderLoadGL()
+// Initialize with gladLoadGL(...) or gladLoaderLoadGL()
+#include <glad/gl.h>
 #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
-#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
-#include <glbinding/Binding.h>  // Initialize with glbinding::Binding::initialize()
+// GLFW including OpenGL headers causes ambiguity or multiple definition errors.
+#define GLFW_INCLUDE_NONE
+// Initialize with glbinding::Binding::initialize()
+#include <glbinding/Binding.h>
 #include <glbinding/gl/gl.h>
 using namespace gl;
 #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
-#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
-#include <glbinding/glbinding.h>// Initialize with glbinding::initialize()
+// GLFW including OpenGL headers causes ambiguity or multiple definition errors.
+#define GLFW_INCLUDE_NONE
+// Initialize with glbinding::initialize()
+#include <glbinding/glbinding.h>
 #include <glbinding/gl/gl.h>
 using namespace gl;
 #else
@@ -53,9 +68,12 @@ namespace window {
     void init() {
 
         // Setup SDL
-        // (Some versions of SDL before <2.0.10 appears to have performance/stalling issues on a minority of Windows systems,
-        // depending on whether SDL_INIT_GAMECONTROLLER is enabled or disabled.. updating to latest version of SDL is recommended!)
-        if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
+        // (Some versions of SDL before <2.0.10 appears to have
+        // performance/stalling issues on a minority of Windows systems,
+        // depending on whether SDL_INIT_GAMECONTROLLER is enabled or
+        // disabled.. updating to latest version of SDL is recommended!)
+        if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER
+                    | SDL_INIT_GAMECONTROLLER) != 0)
         {
             printf("Error: %s\n", SDL_GetError());
             exit(-1);
@@ -66,21 +84,26 @@ namespace window {
         // GL ES 2.0 + GLSL 100
         const char* glsl_version = "#version 100";
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+            SDL_GL_CONTEXT_PROFILE_ES);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 #elif defined(__APPLE__)
         // GL 3.2 Core + GLSL 150
         const char* glsl_version = "#version 150";
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+        // Always required on Mac
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS,
+            SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+            SDL_GL_CONTEXT_PROFILE_CORE);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 #else
         // GL 3.0 + GLSL 130
         const char* glsl_version = "#version 130";
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+                SDL_GL_CONTEXT_PROFILE_CORE);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 #endif
@@ -89,8 +112,11 @@ namespace window {
         SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
         SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
         SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-        SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-        window_ptr = SDL_CreateWindow("Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+        SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL |
+            SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+        window_ptr = SDL_CreateWindow("Dear ImGui SDL2+OpenGL3 example",
+            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720,
+            window_flags);
         gl_context = SDL_GL_CreateContext(window_ptr);
         SDL_GL_MakeCurrent(window_ptr, gl_context);
         SDL_GL_SetSwapInterval(1); // Enable vsync
@@ -103,15 +129,21 @@ namespace window {
 #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
         bool err = gladLoadGL() == 0;
 #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
-        bool err = gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress) == 0; // glad2 recommend using the windowing library loader instead of the (optionally) bundled one.
+        // glad2 recommend using the windowing library loader instead of the
+        // (optionally) bundled one.
+        bool err = gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress) == 0;
 #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
         bool err = false;
         glbinding::Binding::initialize();
 #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
         bool err = false;
-        glbinding::initialize([](const char* name) { return (glbinding::ProcAddress)SDL_GL_GetProcAddress(name); });
+        glbinding::initialize([](const char* name) {
+            return (glbinding::ProcAddress)SDL_GL_GetProcAddress(name);
+        });
 #else
-        bool err = false; // If you use IMGUI_IMPL_OPENGL_LOADER_CUSTOM, your loader is likely to requires some form of initialization.
+        // If you use IMGUI_IMPL_OPENGL_LOADER_CUSTOM, your loader is likely to
+        // requires some form of initialization.
+        bool err = false; 
 #endif
         if (err)
         {
@@ -123,10 +155,14 @@ namespace window {
         IMGUI_CHECKVERSION();
         ImGui::CreateContext();
         ImGuiIO& io = ImGui::GetIO(); (void)io;
-        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
-        //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
-        io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+        // Enable Keyboard Controls
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+        // Enable Gamepad Controls
+        //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+        // Enable Docking
+        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+        // Enable Multi-Viewport / Platform Windows
+        io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
         //io.ConfigViewportsNoAutoMerge = true;
         //io.ConfigViewportsNoTaskBarIcon = true;
 
@@ -134,7 +170,8 @@ namespace window {
         ImGui::StyleColorsDark();
         //ImGui::StyleColorsClassic();
 
-        // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+        // When viewports are enabled we tweak WindowRounding/WindowBg so
+        // platform windows can look identical to regular ones.
         ImGuiStyle& style = ImGui::GetStyle();
         if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
         {
@@ -147,18 +184,44 @@ namespace window {
         ImGui_ImplOpenGL3_Init(glsl_version);
 
         // Load Fonts
-        // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-        // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-        // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-        // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+        // - If no fonts are loaded, dear imgui will use the default font.
+        //   You can also load multiple fonts and use
+        //   ImGui::PushFont()/PopFont() to select them.
+        //
+        // - AddFontFromFileTTF() will return the ImFont* so you can store
+        //   it if you need to select the font among multiple.
+        //
+        // - If the file cannot be loaded, the function will return NULL.
+        //   Please handle those errors in your application (e.g. use an
+        //   assertion, or display an error and quit).
+        //
+        // - The fonts will be rasterized at a given size (w/ oversampling)
+        //   and stored into a texture when calling
+        //   ImFontAtlas::Build()/GetTexDataAsXXXX(), which
+        //   ImGui_ImplXXXX_NewFrame below will call.
+        //
         // - Read 'docs/FONTS.md' for more instructions and details.
-        // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+        //
+        // - Remember that in C/C++ if you want to include a backslash
+        // \ in a string literal you need to write a double backslash \\ !
+        //
         //io.Fonts->AddFontDefault();
-        //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-        //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-        //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-        //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-        //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+        //
+        //io.Fonts->AddFontFromFileTTF(
+        //     "../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+        //
+        //io.Fonts->AddFontFromFileTTF(
+        //  "../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+        //
+        //io.Fonts->AddFontFromFileTTF(
+        //  "../../misc/fonts/DroidSans.ttf", 16.0f);
+        //
+        //io.Fonts->AddFontFromFileTTF(
+        //  "../../misc/fonts/ProggyTiny.ttf", 10.0f);
+        //ImFont* font = io.Fonts->AddFontFromFileTTF(
+        //  "c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f,
+        //  NULL, io.Fonts->GetGlyphRangesJapanese());
+        //
         //IM_ASSERT(font != NULL);
 
     }
@@ -174,8 +237,8 @@ namespace window {
         // Poll and handle events (inputs, window resize, etc.)
         // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to
         // tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to
-        //      your main application.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data
+        //      to your main application.
         // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input
         //      data to your main application.
         // Generally you may always pass all inputs to dear imgui, and hide them
