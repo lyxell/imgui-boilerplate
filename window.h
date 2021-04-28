e3ca48fa7cd9157933a60e80cbfe144934ea3c96
lyxell <alyxell@kth.se>
2021-04-28 23:35:26 +0200

    Fix formatting

--- window.h
+++ window.h
@@ -6,11 +6,11 @@
 #include <string>
 
 namespace window {
-    extern std::queue<SDL_Keysym> keyboard_input;
-    extern std::string text_input;
-    bool is_exiting();
-    void start_frame();
-    void end_frame();
-    void init();
-    void destroy();
-};
+extern std::queue<SDL_Keysym> keyboard_input;
+extern std::string text_input;
+bool is_exiting();
+void start_frame();
+void end_frame();
+void init();
+void destroy();
+}; // namespace window
